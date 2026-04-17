82451c28623ff851676d07cf3f7d13d3306481af
familletenanfouet-cmyk <familletenanfouet@gmail.com>
2026-04-16 23:30:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,41 @@
-# Déclaration de la variable (on ne met pas la valeur ici)
-variable "github_token" {
-  type      = string
-  sensitive = true
+terraform {
+  required_providers {
+    # On garde le provider GitHub
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    # On ajoute le provider Docker
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
 }
 
+# --- Configuration GITHUB ---
 provider "github" {
   token = var.github_token
+}
+
+variable "github_token" {
+  description = "Token pour GitHub"
+  type        = string
+  sensitive   = true
+}
+
+# --- Configuration DOCKER ---
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine" # Indispensable pour Windows
+}
+
+resource "docker_image" "kali" {
+  name = "kalilinux/kali-rolling"
+}
+
+resource "docker_container" "kali_container" {
+  image = docker_image.kali.image_id
+  name  = "mon-kali-docker"
+  stdin_open = true
+  tty        = true
 }
