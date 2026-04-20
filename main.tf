43439990614a7e48b051b333d4b12f6a22fe0594
familletenanfouet-cmyk <familletenanfouet@gmail.com>
2026-04-19 21:58:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
 terraform {
   required_providers {
-    # Configuration du provider GitHub
     github = {
       source  = "integrations/github"
       version = "~> 6.0"
     }
-    # Configuration du provider Docker
     docker = {
       source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
   }
-}
-provider "docker" {
-  host = "tcp://192.168.146.130:2375" # Remplacez par votre IP
 }
 
 # --- Configuration GITHUB ---
@@ -28,8 +23,9 @@ variable "github_token" {
 }
 
 # --- Configuration DOCKER ---
-# On laisse vide pour que Terraform choisisse automatiquement
-# le bon protocole (Unix pour Linux/Pipeline ou npipe pour Windows)
+# On laisse vide pour la détection automatique locale
+provider "docker" {}
+
 # --- Ressources KALI ---
 resource "docker_image" "kali" {
   name         = "kalilinux/kali-rolling"
@@ -60,39 +56,3 @@ resource "docker_container" "ubuntu_target" {
     external = 2222
   }
 }
-
-# --- Configuration WINDOWS SERVER 2019 (VMware) ---
-
-resource "vsphere_virtual_machine" "win_server" {
-  name             = "Mon-Windows-Server-2019"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = 2
-  memory   = 4096
-  guest_id = "windows9Server64Guest"
-
-  network_interface {
-    network_id = data.vsphere_network.network.id
-  }
-
-  disk {
-    label = "disk0"
-    size  = 40
-  }
-
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    
-    # Personnalisation de Windows (Nom, Mot de passe admin, etc.)
-    customize {
-      windows_options {
-        computer_name = "win-srv-2019"
-        admin_password = "TonMotDePasseSecurise123!"
-      }
-    }
-  }
-}
-
-
-
